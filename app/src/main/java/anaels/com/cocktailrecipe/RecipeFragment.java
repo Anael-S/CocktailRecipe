ccc1642296b2f045e56f6b1edf0dd1687457d8bb
Anael <anael.savoyant@gmail.com>
2017-12-31 18:43:19 +0100

    UI enhancements

--- app/src/main/java/anaels/com/cocktailrecipe/RecipeFragment.java
+++ app/src/main/java/anaels/com/cocktailrecipe/RecipeFragment.java
@@ -1,8 +1,6 @@
 package anaels.com.cocktailrecipe;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.app.Fragment;
@@ -11,10 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
